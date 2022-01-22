b216c8f5860f1bf7f944b44f48767f5d29cbd096
Mari <92696334+MarinaSkiba@users.noreply.github.com>
2022-01-22 04:44:10 +0300

    Add files via upload
    file

---
+++ js/anim.js
@@ -0,0 +1,24 @@
+'use strict';
+const country = document.querySelectorAll('.country_container');
+
+window.addEventListener('scroll', checkCountry);
+
+checkCountry();
+
+function checkCountry() {
+  var triggerBottom = window.innerHeight / 5 * 9;
+
+  country.forEach(country_container => {
+    const countryTop = country_container.getBoundingClientRect().top;
+
+    if (countryTop < triggerBottom) {
+      country_container.classList.add('_show');
+    }
+    else {
+      country_container.classList.remove('_show');
+
+    };
+
+  });
+
+}
